90fc885a3f8cc0c654f25b355094186f454d7a9e
nkun0003 <nkun0003@algonquinlive.com>
2024-11-06 23:40:12 -0500

    Deleted the lib file

--- bin/exercise_4.dart
+++ bin/exercise_4.dart
@@ -1,5 +1,1 @@
-import 'package:exercise_4/exercise_4.dart' as exercise_4;
 
-void main(List<String> arguments) {
-  print('Hello world: ${exercise_4.calculate()}!');
-}
--- test/exercise_4_test.dart
+++ test/exercise_4_test.dart
@@ -1,8 +1,1 @@
-import 'package:exercise_4/exercise_4.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
