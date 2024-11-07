43f56bc4e9079dd05fd91fa1159f32547f85a309
nkun0003 <nkun0003@algonquinlive.com>
2024-11-06 23:22:42 -0500

    init the project

---
+++ bin/exercise_4.dart
@@ -0,0 +1,5 @@
+import 'package:exercise_4/exercise_4.dart' as exercise_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercise_4.calculate()}!');
+}
---
+++ test/exercise_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercise_4/exercise_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
